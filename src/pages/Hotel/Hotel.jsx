87eec4316b7643caa0643ff7f2c1cb4dfb0218f1
SuyogLuitel <suyogluitel235@gmail.com>
2024-10-13 11:14:34 +0545

    feat: hotel page completed

--- src/pages/Hotel/Hotel.jsx
+++ src/pages/Hotel/Hotel.jsx
@@ -1,7 +1,168 @@
 import React from "react";
+import HotelCard from "../../components/HotelCard";
+import SelectField from "../../ui/SelectField";
+import LoginInput from "../../ui/LoginInput";
+import { useForm } from "react-hook-form";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { FaStar } from "react-icons/fa";
+import Pagination from "../../components/Pagination";
 
 const Hotel = () => {
-  return <div>Hotel</div>;
+  const { register } = useForm();
+
+  const options = [{ label: "abc", value: "abc" }];
+
+  const facilityList = [
+    { id: 1, label: "Parking", value: "Parking" },
+    { id: 2, label: "Free Wifi", value: "Free Wifi" },
+    { id: 3, label: "Swimming pool", value: "Swimming pool" },
+    { id: 4, label: "Private bathroom", value: "Private bathroom" },
+  ];
+
+  const ratingList = [
+    {
+      id: 1,
+      label: (
+        <div className="flex gap-1 items-center font-normal">
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />( 5 stars )
+        </div>
+      ),
+      value: 5,
+    },
+    {
+      id: 2,
+      label: (
+        <div className="flex gap-1 items-center font-normal">
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />( 4 stars )
+        </div>
+      ),
+      value: 4,
+    },
+    {
+      id: 3,
+      label: (
+        <div className="flex gap-1 items-center font-normal">
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />( 3 stars )
+        </div>
+      ),
+      value: 3,
+    },
+    {
+      id: 4,
+      label: (
+        <div className="flex gap-1 items-center font-normal">
+          <FaStar color="#FBC20B" />
+          <FaStar color="#FBC20B" />( 2 stars )
+        </div>
+      ),
+      value: 2,
+    },
+    {
+      id: 5,
+      label: (
+        <div className="flex gap-1 items-center font-normal">
+          <FaStar color="#FBC20B" />( 1 stars )
+        </div>
+      ),
+      value: 1,
+    },
+  ];
+
+  return (
+    <div className="py-10 px-20 flex flex-col gap-8 bg-secondary">
+      <div className="flex items-center justify-between mb-4">
+        <h2 className="text-2xl font-bold">All Hotels</h2>
+        <LoginInput
+          register={register}
+          name={"search"}
+          labelName={"Search For Property"}
+          placeholder={"Enter property name"}
+          icon={
+            <HiMagnifyingGlass fontSize={28} className="text-[#8E8E93] pr-2" />
+          }
+        />
+      </div>
+      <div className="flex">
+        <div className="w-[30%] pr-10 flex flex-col gap-5">
+          <div className="flex flex-col gap-2">
+            <h2 className="text-xl font-bold text-[#343434]">
+              Filter by property type
+            </h2>
+            <SelectField
+              options={options}
+              placeholder={"Select property type"}
+            />
+          </div>
+          <div className="flex flex-col gap-2 rounded">
+            <h2 className="text-xl font-bold text-[#343434]">
+              Filter by amneties
+            </h2>
+            {facilityList.map((item, index) => (
+              <div key={index} className="flex gap-3 items-center">
+                <input
+                  type="checkbox"
+                  id={item.value}
+                  name={item.value}
+                  value={item.value}
+                  className="w-4 h-4 rounded cursor-pointer"
+                />
+                <label
+                  htmlFor={item.value}
+                  className="text-base font-medium text-[#343434] cursor-pointer"
+                >
+                  {" "}
+                  {item.label}
+                </label>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2 rounded">
+            <h2 className="text-xl font-bold text-[#343434]">
+              Filter by rating
+            </h2>
+            {ratingList.map((item, index) => (
+              <div key={index} className="flex gap-3 items-center">
+                <input
+                  type="checkbox"
+                  id={item.value}
+                  name={item.value}
+                  value={item.value}
+                  className="w-4 h-4 rounded cursor-pointer"
+                />
+                <label
+                  htmlFor={item.value}
+                  className="text-base font-medium text-[#343434] cursor-pointer"
+                >
+                  {" "}
+                  {item.label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="w-[70%] flex flex-col gap-10">
+          <div className="grid grid-cols-3 gap-8">
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+          </div>
+          <div>
+            <Pagination />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hotel;
---
+++ src/ui/SelectField.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Select from "react-select";
+
+export default function SelectField({
+  placeholder,
+  options,
+  className,
+  defaultValue,
+  label,
+  onChange,
+  required,
+  errorMessage,
+}) {
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      height: 34,
+      boxShadow: "none",
+      borderColor: "#9A9A9A",
+      "&:hover": {
+        border: "1px solid #9A9A9A",
+      },
+      fontSize: 14,
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: "#000",
+      fontSize: 14,
+      opacity: 0.5,
+    }),
+    menu: (provided) => ({ ...provided, zIndex: 9999 }),
+    menuPortal: (provided) => ({ ...provided, zIndex: 9999 }),
+  };
+  return (
+    <div className="flex flex-col gap-[6px]">
+      <label className="text-[16px]">
+        {label}
+        {required && <span className="text-red-500 ms-1">*</span>}
+      </label>
+      <div className="flex flex-col">
+        <Select
+          // defaultValue={defaultValue}
+          placeholder={placeholder}
+          options={options}
+          styles={customStyles}
+          onChange={(selectedOption) => onChange(selectedOption?.value)}
+          className={`${className}`}
+          isClearable
+        />
+        {errorMessage && <p className="text-red-600 text-sm">{errorMessage}</p>}
+      </div>
+    </div>
+  );
+}
